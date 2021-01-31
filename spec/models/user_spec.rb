7a4e7156e937814f45d09ee0f5ad08ed6a1cedd1
Julien Bouland <bouland.julien@gmail.com>
2021-01-31 15:34:05 +0100

    User model specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { described_class.new(name: "Julien") }
+
+  context "#initialize" do
+    it { expect(subject).to be_valid }
+
+    it "name should be valid" do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end 
+  end
 end
