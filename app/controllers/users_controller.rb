a70d5a5ffc13c717a3bc467faedf08d1b437ef1c
Julien Bouland <bouland.julien@gmail.com>
2021-01-31 16:39:22 +0100

    user controller done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,42 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
+
+  def index
+    @users = User.all
+  end
+
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path, notice: 'User Created Successfully.'
+    else
+      render :new
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+      redirect_to users_path, notice: 'User has been updated Successfully.'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
 end
